b6b6f4dc97e3d25895ab3a43870af611d4abdaf9
Guidix <guidixm@gmail.com>
2026-05-10 08:06:39 +0300

    peridot: charging: Adjust thermal limits based on fast charge modes
    Change-Id: I0fbc13abd901466d89b353a879d95f68e8e257d9
    Signed-off-by: Guidix <guidixm@gmail.com>

--- configs/battery/charging.cpp
+++ configs/battery/charging.cpp
@@ -8,9 +8,11 @@
 
 #define T_NODE "/sys/class/power_supply/battery/temp"
 #define L_NODE "/sys/class/power_supply/battery/charge_control_limit"
+#define FC_NODE "/sys/class/qcom-battery/fastcharge_enable"
 
 int main() {
     int last_idx = -1;
+    int last_mode = -1;
     while (true) {
         std::ifstream f_t(T_NODE);
         int t = 0;
@@ -20,19 +22,42 @@ int main() {
         }
         f_t.close();
 
-        int idx = 0;
-        if (t >= 440) idx = 16;
-        else if (t >= 410) idx = 10;
-        else if (t >= 380) idx = 5;
-        else idx = 0;
+        std::ifstream f_fc(FC_NODE);
+        int fast_charge_mode = 1; // Default to fast charge
+        if (f_fc >> fast_charge_mode) {
+            f_fc.close();
+        }
 
-        if (idx != last_idx) {
+        int idx = 0;
+
+        if (fast_charge_mode == 0) {
+            // value_none
+            if (t >= 370) idx = 16;
+            else if (t >= 360) idx = 16;
+            else if (t >= 350) idx = 10;
+            else idx = 5;
+        } else if (fast_charge_mode == 1) {
+            // value_fast_charge
+            if (t >= 410) idx = 16;
+            else if (t >= 390) idx = 10;
+            else if (t >= 370) idx = 5;
+            else idx = 0;
+        } else if (fast_charge_mode == 2) {
+             // value_super_fast_charge
+             if (t >= 440) idx = 16;
+             else if (t >= 410) idx = 10;
+             else if (t >= 380) idx = 5;
+             else idx = 0;
+        }
+
+        if (idx != last_idx || fast_charge_mode != last_mode) {
             std::ofstream f_l(L_NODE);
             if (f_l.is_open()) {
                 f_l << idx;
                 f_l.close();
-                LOGI("%d.%dC | idx: %d", t/10, t%10, idx);
+                LOGI("%d.%dC | FC Mode: %d | idx: %d", t/10, t%10, fast_charge_mode, idx);
                 last_idx = idx;
+                last_mode = fast_charge_mode;
             }
         }
         std::this_thread::sleep_for(std::chrono::seconds(5));
