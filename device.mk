77bd65093c1f1c3f3a229a5c9d6f26edbb18f15d
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2026-05-12 20:07:10 +0000

    gayy

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/dolby/dax-default-spatializer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default-spatializer.xml \
 
 PRODUCT_PACKAGES += \
-    XiaomiDolby
+    LunarisDolby
 
 # DRM
 PRODUCT_PACKAGES += \
