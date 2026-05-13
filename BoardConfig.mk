7f409d7bc71f1649d2721468ea671e3951163b05
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2026-05-13 22:57:17 +0000

    miuicam

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,8 +9,6 @@ DEVICE_PATH := device/xiaomi/peridot
 TARGET_USES_VULKAN := true
 
 # MiuiCamera
-CAMERA_PACKAGE_NAME := com.android.camera
-
 
 # Inherit from proprietary files for miuicamera
 -include device/xiaomi/peridot-miuicamera/BoardConfig.mk
