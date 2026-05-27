36f211c8a2961e735f93d7aa12c4a0a75c091dbe
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2026-05-27 19:56:21 +0000

    peridot: bringup evo

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common EvolutionX stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from peridot device
@@ -17,16 +17,10 @@ $(call inherit-product, device/xiaomi/peridot/device.mk)
 PRODUCT_NAME := lineage_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := 24069PC21G
 
-PRODUCT_SYSTEM_NAME := peridot_global
-PRODUCT_SYSTEM_DEVICE := peridot
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="peridot_global-user 16 BP2A.250605.031.A3 OS3.0.6.0.WNPMIXM release-keys" \
-    BuildFingerprint=POCO/peridot_global/peridot:16/BP2A.250605.031.A3/OS3.0.6.0.WNPMIXM:user/release-keys \
-    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
-    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
-
+PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Flags
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_HAS_UDFPS := true
