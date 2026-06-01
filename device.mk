14c28586838bca0e915d21ae2cc00708f55e2a85
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2026-06-01 10:50:12 +0000

    peridot: Remove aosp hbm display config Allows the panel to go maximum brightness directly from qs brightness slider

--- device.mk
+++ device.mk
@@ -142,11 +142,6 @@ PRODUCT_PACKAGES += \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh
 
-$(foreach display_id, 4630947195234848131 4630947033261136259 4630946545580055171, \
-    $(eval PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_$(display_id).xml \
-    ))
-
 # Charging
 PRODUCT_PACKAGES += \
     charging
@@ -157,7 +152,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/dolby/dax-default-spatializer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default-spatializer.xml \
 
 PRODUCT_PACKAGES += \
-    LunarisDolby
+    DolbyAtmos
 
 # DRM
 PRODUCT_PACKAGES += \
